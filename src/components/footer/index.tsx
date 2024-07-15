1d4bf084d15e536b06f2d7d2a535e273a654f2b0
kristechhub <kmj.algario@gmail.com>
2024-07-15 14:40:19 +0800

    finalized for deployment

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <footer className="bg-primary-100 py-16">
             <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
                 <div className="mt-16 basis-1/2 md:mt-0">
-                <img alt="logo" src={Logo} className="md:w-[150px]" />
+                <img alt="logo" src={Logo} className="w-[150px] md:w-[200px]" />
                     <p className="my-5">
                         Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
                         purus et arcu massa dictum condimentum. Augue scelerisque iaculis
